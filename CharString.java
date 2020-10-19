0f0fb5bace4d758440af2b0888905c608ed50063
cshwan23 <67510904+cshwan23@users.noreply.github.com>
2020-10-19 22:16:20 +0900

    Add files via upload

---
+++ CharString.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	
+	//문자 = ' ' , 문자열 = " "
+		
+	public static void main(String[] args) {
+		System.out.println("생활코딩"+"입니다");
+		System.out.println("생");
+		System.out.println("1"+"1");
+		//\역슬래시"
+		System.out.println("egoing said \"Welcome programming world\"");
+		//\n 줄바꿈
+		System.out.println("egoing said\n \"Welcome programming world\"");
+
+	}
+
+}
